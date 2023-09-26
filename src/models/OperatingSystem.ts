6a6b9baa27021d09e9cc13a9462c260cb0b86273
whiskey-actual <actualwhiskey@gmail.com>
2023-09-26 10:49:14 -0400

    work on OS

--- src/models/OperatingSystem.ts
+++ src/models/OperatingSystem.ts
@@ -1,169 +1,68 @@
-import { LogEngine } from 'whiskey-log';
 import { Utilities } from 'whiskey-util'
-
-import { getUnifiedObject } from "../components/getUnifiedObject";
-
-
-import mongoose, { model, Schema } from "mongoose";
-
-export class OperatingSystem {
-
-  constructor(le:LogEngine, operatingSystemLabel:string) {
-
-    this._le = le
-    this.osSourceLabel = operatingSystemLabel
-
-    let tempLabel:string = operatingSystemLabel
-
-      if(Utilities.doesRegexMatch(tempLabel, ['/microsoft/gi','/windows/gi'])) {
-          this.osVendor = "Microsoft"
-          this.osPlatform = "Windows"
-        }
-
-        //now remove the matches
-        tempLabel = tempLabel.replace('microsoft', '');
-        tempLabel = tempLabel.replace('windows', '')
-        tempLabel = tempLabel.trim();
-
-        if(Utilities.doesRegexMatch(tempLabel, ['/server/gi'])) {
-          this.osClass = "Server"
-        } else {
-          this.osClass = "End-user device"
-        }
-
-        tempLabel = tempLabel.replace('server','')
-
-        this.osLabelRemainder = tempLabel
-        
-  }
-
-  private _le:LogEngine = new LogEngine([])
-  public readonly osSourceLabel:string = ''
-  public readonly osVendor:string = ''
-  public readonly osPlatform:string = ''
-  public readonly osClass:string = ''
-  public readonly osLabelRemainder:string = ''
-  public readonly osVersion:string = ''
-  public readonly osVariant:string = ''
-
-  public async saveOperatingSystem():Promise<void> {
-
-    this._le.logStack.push('saveOperatingSystem')
-
-    try {
-
-      let o:OperatingSystem.OperatingSystemInterface = {
-        osSourceLabel: this.osSourceLabel,
-        osVendor: this.osVendor,
-        osPlatform: this.osPlatform,
-        osVersion: this.osVersion,
-        osVariant: this.osVariant,
-        osClass: this.osClass, 
-        osLabelReminder: this.osLabelRemainder
-      }
-
-      const unifiedObject:any = await getUnifiedObject(this._le, 'OperatingSystem', 'osSourceLabel', this.osSourceLabel, o);
-
-      const os = await mongoose.model('OperatingSystem').findOneAndUpdate(
-        { osSourceLabel: o.osSourceLabel },
-        { $set: unifiedObject},
-        {
-          new: true,
-          upsert: true
-        }
-      );
-
-      console.debug(os);
-
-    } catch(err:any) {
-      this._le.AddLogEntry(LogEngine.Severity.Error, LogEngine.Action.Note, err)
-      throw(err)
-    } finally {
-      this._le.logStack.pop();
-    }
-
-    return new Promise<void>((resolve) => {resolve()})
-
-  }
-}
+import { model, Schema } from "mongoose";
 
 
-export namespace OperatingSystem {
-
-  export interface OperatingSystemInterface {
-    osSourceLabel: string;
-    osClass: string;
-    osVendor: string;
-    osPlatform: string;
-    osVersion: string;
-    osVariant: string;
-    osLabelReminder: string;
+  export interface OperatingSystem {
+    osLabel: string;
   }
 
   export const OperatingSystemSchema = new Schema({
-    osSourceLabel: {type:String, required:true, index:true, id: true, unique: true},
+    osLabel: {type:String, required:true, index:true, id: true, unique: true},
     osClass: {type:String, index:true },
     osVendor: {type:String, index:true },
     osPlatform: {type:String, index:true },
     osVersion: {type:String, index:true },
     osVariant: {type:String, index:true },
-
+    osLabelRemainder: {type:String, index:true}
   }, {
     collection: 'operatingSystems',
     timestamps: true,
     autoCreate: true,
-  })
+    virtuals: {
+      osLabel: {
+        async set(v) {
+          // `v` is the value being set, so use the value to set
+          // `firstName` and `lastName`.{
+          let tempLabel:string = this.osLabel
 
-  export const OperatingSystemModel = model('OperatingSystem', OperatingSystemSchema)
+          if(Utilities.doesRegexMatch(tempLabel, ['/microsoft/gi','/windows/gi'])) {
+            this.set({ osVendor: 'Microsoft', osPlatform: 'Windows'});
+          }
+
+          //now remove the matches
+          tempLabel = tempLabel.replace('microsoft', '');
+          tempLabel = tempLabel.replace('windows', '')
+          tempLabel = tempLabel.trim();
+
+          if(Utilities.doesRegexMatch(tempLabel, ['/server/gi'])) {
+            this.set({osClass: "Server"})
+          } else {
+            this.set({osClass: "End-user device"})
+          }
+
+          tempLabel = tempLabel.replace('server','')
+
+          this.set({osLabelRemainder: tempLabel})
+
+        }
+      }
+    },
+    methods: {
+      async saveOS() {
+        await model('OperatingSystem').updateOne(
+          { osLabel: this.osLabel },
+          {
+            $set: this
+          },
+          {
+            new: true,
+            upsert: true
+          }
+        );
+      }
+    }
+  });
 
 
-  // Virtuals
-  // DeviceSchema.virtual("type").get(function(this: Device) {
-  //   //return this.deviceType
-  // })
 
-  // Methods
-  // DeviceSchema.methods.getOperatingSystem = function(this: Device) {
-  //   //return this.operatingSystem
-  // }
-
-  // Document middlewares
-  // OperatingSystemSchema.pre<OperatingSystem>("updateOne", async function() {
-
-  //   let tempString = this.osSourceLabel;
-
-  //   if(!tempString===undefined) {
-
-  //     if(tempString.match('/microsoft/i') || tempString.match('/windows/i')) {
-  //       this.osVendor = "microsoft"
-  //       this.osPlatform = "windows"
-  //     }
-    
-  //     //now remove the matches
-  //     tempString = tempString.replace('microsoft', '');
-  //     tempString = tempString.replace('windows', '')
-  //     tempString = tempString.trim();
-    
-  //     if(this.osSourceLabel.match('/server/i')) {
-  //       this.osClass = "server"
-  //     } else {
-  //       this.osClass = "end-user device"
-  //     }
-    
-  //     tempString = tempString.replace('server','')
-    
-
-  //   }
-
-
-  //   // prune objects
-  //   // if(!this.observedByActiveDirectory) {delete this.observedByActiveDirectory}
-  //   // if(!this.observedByAzure) {delete this.observedByAzure}
-  //   // if(!this.observedByAzureMDM) {delete this.observedByAzureMDM}
-  //   // if(!this.observedByCrowdstrike) {delete this.observedByCrowdstrike}
-
-  // });
-
-  
-
-}
+  export const OperatingSystem = model('OperatingSystem', OperatingSystemSchema)
