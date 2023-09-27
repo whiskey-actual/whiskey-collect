c47b6b383fc887285b17856ffb4c8ba774f3c34f
whiskey-actual <actualwhiskey@gmail.com>
2023-09-27 16:26:01 -0400

    tweak aad

--- src/components/DBEngine.ts
+++ src/components/DBEngine.ts
@@ -172,7 +172,7 @@ export class DBEngine {
                 let changeDetected:boolean = false
                 if(changeDetection) {
                     currentValue = await this.getSingleValue(updatePackage.tableName, updatePackage.idColumn, updatePackage.UpdatePackageItems[i].idValue, updatePackage.UpdatePackageItems[i].updateColumn);
-                    if(currentValue.toString().trim()!=updatePackage.UpdatePackageItems[i].updateValue.toString().trim()) {
+                    if(!currentValue || currentValue.toString().trim()!=updatePackage.UpdatePackageItems[i].updateValue.toString().trim()) {
                         changeDetected=true
                     }
                 }
