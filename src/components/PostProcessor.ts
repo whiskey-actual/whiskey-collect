54a245ce868f2d5307f10b13f589b7fdaed0e1ac
Whiskey Actual <actualwhiskey@gmail.com>
2023-10-10 08:38:18 -0400

    begin adding postProcessor()

--- src/components/PostProcessor.ts
+++ src/components/PostProcessor.ts
@@ -11,33 +11,37 @@ export class PostProcessor {
       private db:DBEngine
 
       public async updateDeviceDetails() {
+        this.le.logStack.push("updateDeviceDetails")
 
-        const devices:mssql.IRecordSet<any> = await this.db.selectColumns("Device", [
-            "DeviceID",
-            "DeviceActiveDirectoryID",
-            "DeviceAzureActiveDirectoryID",
-            "DeviceAzureManagedID",
-            "DeviceConnectwiseID",
-            "DeviceCrowdstrikeID"
-        ], [])
+        try {
 
-        for(let i=0; i<devices.length; i++) {
-
-            const matchCondition:ColumnValuePair = new ColumnValuePair("DeviceActiveDirectoryID", devices[i].DeviceActiveDirectoryID, mssql.Int)
-
-            const datetimes = await this.db.selectColumns("DeviceActiveDirectory", [
-                "activeDirectoryWhenCreated",
-                "activeDirectoryWhenChanged",
-                "activeDirectoryLastLogon",
-                "activeDirectoryPwdLastSet",
-                "activeDirectoryLastLogonTimestamp"
-            ], [matchCondition])
-
-            console.debug(datetimes)
-
+            const devices:mssql.IRecordSet<any> = await this.db.selectColumns("Device", [
+                "DeviceID",
+                "DeviceActiveDirectoryID",
+                "DeviceAzureActiveDirectoryID",
+                "DeviceAzureManagedID",
+                "DeviceConnectwiseID",
+                "DeviceCrowdstrikeID"
+            ], [])
+    
+            for(let i=0; i<devices.length; i++) {
+    
+                const matchCondition:ColumnValuePair = new ColumnValuePair("DeviceActiveDirectoryID", devices[i].DeviceActiveDirectoryID, mssql.Int)
+    
+                const datetimes = await this.db.selectColumns("DeviceActiveDirectory", [
+                    "activeDirectoryWhenCreated",
+                    "activeDirectoryWhenChanged",
+                    "activeDirectoryLastLogon",
+                    "activeDirectoryPwdLastSet",
+                    "activeDirectoryLastLogonTimestamp"
+                ], [matchCondition])
+    
+                console.debug(datetimes)
+    
+            }
+        } catch(err) {
+            this.le.AddLogEntry(LogEngine.Severity.Error, LogEngine.Action.Note, `${err}`)
+            throw(err);
         }
-
-
-
       }
 }
